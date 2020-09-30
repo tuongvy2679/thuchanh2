8f483e0d9aa468dae278bc5af5bfb752bccdf89f
tuongvy2679 <70678490+tuongvy2679@users.noreply.github.com>
2020-09-30 15:44:42 +0700

    update

--- src/app/header2/header2.component.ts
+++ src/app/header2/header2.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-header2',
   templateUrl: './header2.component.html',
   styleUrls: ['./header2.component.css']
+  
 })
 export class Header2Component implements OnInit {
 
